b29f0dc953772390dabf7396ce6fc5085099b6a6
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-02-03 18:54:38 -0800

    using gorm to conenct with database now

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
 
@@ -19,16 +19,11 @@ const (
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
 
 	fmt.Println("Successfully connected!")
 	// Set the router as the default one shipped with Gin
