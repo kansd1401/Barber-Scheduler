0a41e25c135908ec4d3273d46a576159a402e5a3
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-02-12 18:01:21 -0800

    added some text to it

--- client/src/components/DayList.js
+++ client/src/components/DayList.js
@@ -6,6 +6,8 @@ export default function DayList(props) {
 
   return (
     <div>
+       <h3 className="text--light">Today</h3>
+       <div>
       {props.days.map((day) => <DayListItem
         key={day.id} 
         name={day.name} 
@@ -13,6 +15,7 @@ export default function DayList(props) {
         selected={day.name === props.day}
         setDay={() => props.setDay(day.name)}
         />)}
+        </div>
     </div>
   );
 }
