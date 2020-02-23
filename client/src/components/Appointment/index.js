1236944630db688540772bb904ec2161c2a76478
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-02-23 14:39:51 -0800

    added show to index

--- client/src/components/Appointment/index.js
+++ client/src/components/Appointment/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Empty from "./empty"
 import Header from "./header"
+import Show from "./show"
 import "./styles.scss"
 
 
@@ -8,8 +9,12 @@ export default function Appointment(props){
   return (
     <article className="appointment">
       <Header time={props.time} />
-      <Empty onAdd={() => console.log("clicked")} />
+      {!props.appointment && <Empty onAdd={() => console.log("clicked")} />}
+      {props.appointment && <Show 
+      name={props.appointment.name}
+      notes={props.appointment.notes}
+      onEdit={props.onEdit}
+      onDelete={props.onDelete}  />}
     </article>
-    
   )
 }
