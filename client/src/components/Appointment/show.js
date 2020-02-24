67ad9233fbb631326e7c69b66db881a92c7f047d
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-02-24 02:05:37 -0800

    each appointment card gets a random color

--- client/src/components/Appointment/show.js
+++ client/src/components/Appointment/show.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Show(props) {
   return (
-    <main className="appointment__card appointment__card--show">
+    <main className={`appointment__card appointment__card--show${Math.floor(Math.random() * (5 - 1 + 1)) + 1}`}>
       <section className="appointment__card-left">
         <h2 className="text--regular">{props.name}</h2>
         <section className="interviewer">
