40c4426fef4482b7225dfd681cdfa2a0475f8ffb
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-02-25 15:01:28 -0800

    date list works and added to app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import axios from 'axios';
 import DayList from './DayList'
 import { func } from 'prop-types';
+
 const days = [];
 
 function daysData(day){
   var weekDays = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-  for(i=1;i<=7;i++){
-
+  for(let i=1;i<=7;i++){
+    days.push({id: i,name: weekDays[day.getDay()], date: day.getDate()})
+    day.setDate(day.getDate() + 1)
   }
 }
 
 function App() {
   let barbersForDay;
   let currentDay = "06-02-2020"
+  let date = new Date('February 06, 2020 03:24:00')
+  daysData(date)
+  console.log(days)
   axios.get('http://localhost:8000/dayData',{
     params:{
       date: currentDay
