fad7e947d20c8e08ac4a182c3eeb54fbd3586b14
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-03-04 19:07:46 -0800

    cssing

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact(props) {
   return (
     <div className="contact__container">
       <img src="/images/image1.jpg"/>
-      <h3>YOooooooooo</h3>
+      <div className="contact__container--info">YOooooooooo</div>
     </div>
   );  
 }
