9d856d1c6280ecb2955cb34763713fd729b46b4d
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-03-04 19:21:48 -0800

    cssing and some refactoriing of css varialbles

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -4,9 +4,22 @@ import "./Contact.scss"
 export default function Contact(props) {
 
   return (
-    <div className="contact__container">
+    <div className="contact">
       <img src="/images/image1.jpg"/>
-      <div className="contact__container--info">YOooooooooo</div>
+      <div className="contact__info">
+        <div className="contact__info--box">
+          <h1>ADDRESS:</h1>
+          <h3>1234 99th Ave Vancouver BC.</h3>
+        </div>
+        <div className="contact__info--box">
+          <h1>TELEPHONE</h1>
+          <h3>204-010-0101</h3>
+        </div>
+        <div className="contact__info--box">
+          <h1>EMAIL:</h1>
+          <h3>barbershop@hmail.com</h3>
+        </div>
+      </div>
     </div>
   );  
 }
